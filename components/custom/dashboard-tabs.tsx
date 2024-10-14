db8d47773888b1d00a8df324a865763df9c74510
Sheel Taskar <sheeltaskar64@gmail.com>
2024-10-14 18:01:09 -0400

    responsive dashboard section

--- components/custom/dashboard-tabs.tsx
+++ components/custom/dashboard-tabs.tsx
@@ -1,29 +1,28 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { ChartColumn } from 'lucide-react';
-import ParallexCard from '../custom/parallex-card';
+import ParallexCard from './parallex-card';
+
 
 export default function DashboardTabs() {
   const images = ['/dashboard.png'];
   return (
-    <Tabs defaultValue="/dashboard.png" className="flex h-full w-full flex-col items-center justify-center">
-      <TabsList className="w-1/4 space-x-12">
+    <Tabs defaultValue="/dashboard.png" className="flex h-full w-full flex-col items-center">
+      <TabsList className="w-1/4 space-x-12 mb-4">
         <TabsTrigger value="/dashboard.png" className="flex flex-col space-y-1 pb-4">
           <ChartColumn />
           <span className="text-xs">Dashboard</span>
         </TabsTrigger>
       </TabsList>
 
-      <div className="p-1">
         {images.map((image) => {
           return (
-            <TabsContent value={image} key={image}>
-              <div className="w-full">
+            <TabsContent value={image} key={image} className='flex justify-center'>
+              <div className="animate-appear w-full md:w-3/4">
                 <ParallexCard src={image} />
               </div>
             </TabsContent>
           );
         })}
-      </div>
     </Tabs>
   );
 }
--- components/sections/Dashboard.tsx
+++ components/sections/Dashboard.tsx
@@ -1,6 +1,7 @@
 import { BellRing, ChartColumn, MoveRight } from 'lucide-react';
 import DashboardTabs from '../custom/dashboard-tabs';
 
+
 export default function Dashboard() {
   const heading = 'Financial Reporting for Informed Decisions';
   const subheading = "Gain insights into your institute's financial health with detailed reports and analyses that keep you on track.";
@@ -12,10 +13,10 @@ export default function Dashboard() {
   ];
 
   return (
-    <div id="dashboard" className="mx-auto mt-32 flex min-h-[525px] max-w-7xl px-10">
-      <div className="w-1/2 pr-16">
-        <div className="text-4xl font-extrabold leading-tight">{heading}</div>
-        <div className="mt-8 text-sm text-muted-foreground">{subheading}</div>
+    <div id="institute" className="mx-auto mt-28 flex min-h-[500px] max-w-7xl flex-col lg:max-h-[500px] lg:flex-row">
+      <div className="flex w-full flex-col items-center py-4 px-4 lg:pr-0 lg:pl-16 lg:w-5/12 lg:items-start">
+        <div className="text-center text-3xl font-extrabold leading-tight md:text-4xl lg:text-left">{heading}</div>
+        <div className="mt-4 text-center text-sm text-muted-foreground lg:text-left">{subheading}</div>
 
         {/* Map over points to generate the content */}
         <div className="my-8 space-y-4 text-sm">
@@ -23,14 +24,14 @@ export default function Dashboard() {
             const IconComponent = point.icon;
             return (
               <div key={index} className="flex items-center">
-                <IconComponent className="mr-3 inline text-primary" size={20} />
+                <IconComponent className="mr-3 inline h-[20px] max-h-[20px] min-h-[20px] w-[20px] min-w-[20px] text-primary" size={20} />
                 {point.text}
               </div>
             );
           })}
         </div>
 
-        <div className="group flex cursor-pointer items-center text-primary">
+        <div className="group mb-12 mt-4 flex cursor-pointer items-center text-primary">
           See in Action
           <MoveRight
             className="ml-3 inline transform transition-transform duration-300 ease-in-out group-hover:translate-x-2"
@@ -38,8 +39,8 @@ export default function Dashboard() {
           />
         </div>
       </div>
-      <div className="w-1/2">
-        <DashboardTabs></DashboardTabs>
+      <div className="w-full lg:w-7/12">
+        <DashboardTabs />
       </div>
     </div>
   );
