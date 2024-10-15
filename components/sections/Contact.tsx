32df4b9fcb887f3c71f33c740871873a874288df
Sheel Taskar <sheeltaskar64@gmail.com>
2024-10-14 20:19:04 -0400

    add contact section

--- components/sections/Contact.tsx
+++ components/sections/Contact.tsx
@@ -7,8 +7,8 @@ import { Label } from '@/components/ui/label';
 export default function Contact() {
   const subheading = 'Reach out to our team to see how your agency can benefit from Tutx.';
   return (
-    <div className="mx-auto flex max-w-7xl px-16 py-36" id="demo">
-      <div className="space-y-10 pr-20">
+    <div className="mx-auto flex flex-col md:flex-row max-w-7xl my-24 sm:px-6" id="demo">
+      <div className="space-y-10 w-full md:w-1/2 p-4 text-center md:text-left">
         <div className="font-bold text-primary">BOOK A DEMO</div>
 
         <div className="leading-tighter text-6xl font-extrabold">
@@ -17,12 +17,12 @@ export default function Contact() {
           <span className="text-primary">Answers</span>
         </div>
         <div className="text-sm text-muted-foreground">{subheading}</div>
-        <div className="text-bold flex items-center text-primary">
-          <MessageCircle className="mr-3 inline" size={20} strokeWidth={1.5} />
+        <div className="text-bold text-primary text-center md:text-left">
+          <MessageCircle className="mr-3 mb-1 inline" size={20} strokeWidth={1.5} />
           hello@teamtutx.com
         </div>
       </div>
-      <div className="flex-1 items-center px-16">
+      <div className="flex-1 items-center pt-8 p-4 w-full md:w-1/2">
         <form className="space-y-4">
           <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
             <div>
@@ -58,7 +58,7 @@ export default function Contact() {
           </div>
 
           <Button type="submit" className="w-full text-background">
-            Submit
+            Book a Demo
           </Button>
         </form>
       </div>
